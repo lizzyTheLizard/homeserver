79016a1520073c8d30a77121db1404d3669a130b
Graf <matthias.graf@zuehlke.com>
2025-10-29 13:46:02 +0100

    Added chromatic

--- webcomponents/.storybook/main.ts
+++ webcomponents/.storybook/main.ts
@@ -2,7 +2,11 @@ import type { StorybookConfig } from '@storybook/web-components-vite'
 const config: StorybookConfig = {
   framework: '@storybook/web-components-vite',
   stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: ['@storybook/addon-docs', '@storybook/addon-vitest'],
+  addons: [
+    '@storybook/addon-docs',
+    '@storybook/addon-vitest',
+    '@chromatic-com/storybook'
+  ],
   docs: { },
 }
 
