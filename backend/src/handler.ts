756c42a6ba4c55ee2653912a331e364203d5d584
lizzyTheLizard <14069652+lizzyTheLizard@users.noreply.github.com>
2025-11-11 19:54:21 +0100

    Fix deployment 11

--- backend/src/handler.ts
+++ backend/src/handler.ts
@@ -1,7 +1,7 @@
 export function handler(event: Record<string, unknown>, context: Record<string, unknown>, cb: unknown) {
   console.log('Function invoked with event:', event, 'and context:', context, 'and callback:', cb)
   return {
-    body: 'Hello World!!!!',
+    body: 'Hello World!!!!!',
     headers: { 'Content-Type': ['application/json'] },
     statusCode: 200,
   }
