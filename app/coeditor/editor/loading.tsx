2cd7f53c773be50d98ab9759bda0d113989e064f
lizzyTheLizard <14069652+lizzyTheLizard@users.noreply.github.com>
2025-12-26 22:29:47 +0100

    Fix Sidebar and some titles

--- app/coeditor/editor/loading.tsx
+++ app/coeditor/editor/loading.tsx
@@ -15,9 +15,10 @@ export default function Loading() {
   }
 
   return (
-    <>
+    <main>
+      <h1>CoEditor</h1>
       <LoadingSpinner></LoadingSpinner>
       <Editor discussion={undefined} templates={[dummyTemplate]} />
-    </>
+    </main>
   )
 }
--- app/coeditor/settings/loading.tsx
+++ app/coeditor/settings/loading.tsx
@@ -3,9 +3,10 @@ import { Settings } from './Settings'
 
 export default function Loading() {
   return (
-    <>
+    <main>
+      <h1>Settings</h1>
       <LoadingSpinner></LoadingSpinner>
       <Settings profiles={[]} templates={[]} />
-    </>
+    </main>
   )
 }
