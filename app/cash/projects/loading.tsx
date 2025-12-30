e5e93afa4cbad6724f63a0c7c4343fd4a3503062
lizzyTheLizard <14069652+lizzyTheLizard@users.noreply.github.com>
2025-12-31 00:58:19 +0100

    Added projects page

---
+++ app/cash/projects/loading.tsx
@@ -0,0 +1,10 @@
+import { LoadingSpinner } from '@/app/shared/components/LoadingSpinner'
+
+export default function Loading() {
+  return (
+    <main>
+      <h1>Projects</h1>
+      <LoadingSpinner />
+    </main>
+  )
+}
---
+++ app/cash/projects/page.tsx
@@ -0,0 +1,33 @@
+import { Metadata } from 'next/dist/lib/metadata/types/metadata-interface'
+import { getUserSession } from '@/app/common/auth/auth'
+import { nontransactional } from '@/app/shared/db'
+import { Card } from '@/app/shared/components/Card'
+import { findProjectsByOwner, Project } from '../Project'
+
+export const metadata: Metadata = {
+  title: 'Cash - Projects',
+}
+
+export default async function Page() {
+  const user = await getUserSession()
+  if (!user) throw new Error('User not authenticated')
+  if (!(user.applications.includes('cash'))) throw new Error('User not authorized for cash application')
+  const projects = await nontransactional(c => findProjectsByOwner(c, user.sub))
+
+  return (
+    <main>
+      <h1>Projects</h1>
+      <div className="row">
+        {projects.map(p => (
+          <Card href={getLink(p)} key={p.id}>
+            <h2>{p.name}</h2>
+          </Card>
+        ))}
+      </div>
+    </main>
+  )
+}
+
+function getLink(project: Project): string {
+  return `/cash/${project.id}/LATEST/journal`
+}
