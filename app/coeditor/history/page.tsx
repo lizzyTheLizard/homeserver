44b1e4e6a13e62eac33b6a614c502502270b429e
lizzyTheLizard <14069652+lizzyTheLizard@users.noreply.github.com>
2026-01-02 17:56:45 +0100

    Refactor History

--- app/coeditor/history/page.tsx
+++ app/coeditor/history/page.tsx
@@ -2,7 +2,10 @@ import { Metadata } from 'next/dist/lib/metadata/types/metadata-interface'
 import { getAuthenticatedUserSession } from '@/app/common/auth/auth'
 import { nontransactional } from '@/app/shared/db'
 import { findDiscussionByOwner } from '../Discussion'
-import { History } from './History'
+import { DataTable } from '@/app/shared/components/DataTable'
+import Link from 'next/dist/client/link'
+import { DateTime } from '@/app/shared/components/DateTime'
+import style from './page.module.css'
 
 export const metadata: Metadata = {
   title: 'CoEditor - History',
@@ -15,7 +18,26 @@ export default async function Page() {
   return (
     <main>
       <h1>History</h1>
-      <History discussions={discussions} />
+      <DataTable className={style.table}>
+        <thead>
+          <tr>
+            <th className={style.title}>Title</th>
+            <th className={style.updated}>Last Updated</th>
+            <th>Text</th>
+            <th>Context</th>
+          </tr>
+        </thead>
+        <tbody>
+          {discussions.map(discussion => (
+            <tr key={discussion.id}>
+              <td><Link href={`/coeditor/editor?id=${discussion.id}`}>{discussion.title}</Link></td>
+              <td><DateTime date={discussion.updated_at} /></td>
+              <td className={style.text}>{discussion.text}</td>
+              <td className={style.text}>{discussion.context}</td>
+            </tr>
+          ))}
+        </tbody>
+      </DataTable>
     </main>
   )
 }
