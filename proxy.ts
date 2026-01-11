48a2d33784e307e5ee3fdb061ff6b4ca25986c68
lizzyTheLizard <14069652+lizzyTheLizard@users.noreply.github.com>
2026-01-11 11:02:36 +0100

    Remove crawlerDetection and unneeded logs

--- proxy.ts
+++ proxy.ts
@@ -2,11 +2,9 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 import { getUserSession, startLogin } from './app/common/auth/auth'
 import { logger } from '@/app/shared/logger'
-import { Crawler } from 'es6-crawler-detect/dist/lib/crawler'
 
 export async function proxy(request: NextRequest): Promise<NextResponse> {
   const session = await getUserSession()
-  const crawlerDetector = new Crawler(request)
 
   let response: NextResponse
   if (request.nextUrl.pathname.startsWith('/_next/')) {
@@ -18,22 +16,8 @@ export async function proxy(request: NextRequest): Promise<NextResponse> {
     response = NextResponse.next()
   else if (request.headers.get('X-Requested-With') === 'XMLHttpRequest')
     response = new NextResponse('Unauthorized', { status: 401 })
-  else if (crawlerDetector.isCrawler()) {
-    const crawler = crawlerDetector.getMatches()
-    const crawlerName = typeof crawler === 'string' ? crawler : 'unknown'
-    logger.info(`Blocked crawler '${crawlerName}' access to ${request.nextUrl.pathname}`)
-    response = new NextResponse('Forbidden', { status: 403 })
-  }
   else if (request.headers.get('User-Agent')?.includes('(Windows NT 6.1; WOW64)')) {
-    logger.info(`Unauthenticated access from Win7 client`)
-    logger.info(`Request details:`)
-    logger.info(`IP: ${request.url}`)
-    logger.info(`Path: ${request.nextUrl.pathname}`)
-    logger.info(`Method: ${request.method}`)
-    logger.info(`Headers:`)
-    request.headers.forEach((value, key) => {
-      logger.info(`Header: ${key} = ${value}`)
-    })
+    logger.info(`Unauthenticated access from Win7 client (probably f6.ru) with IP ${request.headers.get('X-Forwarded-For') ?? 'unknown'}`)
     response = new NextResponse('Forbidden', { status: 403 })
   }
   else {
