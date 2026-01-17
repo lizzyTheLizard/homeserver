7d65d91471ffcf273b8c1b4b44c864efb2616704
lizzyTheLizard <14069652+lizzyTheLizard@users.noreply.github.com>
2026-01-17 22:39:15 +0100

    Reduce system resources but skip shutdown if f6

--- proxy.ts
+++ proxy.ts
@@ -3,13 +3,9 @@ import type { NextRequest } from 'next/server'
 import { getUserSession, startLogin } from './app/common/auth/auth'
 import { logger } from '@/app/shared/logger'
 
-let lastNormalRequestTime: Date | undefined = undefined
-const MAX_IDLE_TIME_MS = 1 * 60 * 1000 // 1 minute
-
 export async function proxy(request: NextRequest): Promise<NextResponse> {
   const session = await getUserSession()
   const isF6Request = request.headers.get('User-Agent')?.includes('(Windows NT 6.1; WOW64)')
-  if (!isF6Request) lastNormalRequestTime = new Date()
   let response: NextResponse
   if (request.nextUrl.pathname.startsWith('/_next/'))
     return NextResponse.next()
@@ -20,20 +16,8 @@ export async function proxy(request: NextRequest): Promise<NextResponse> {
   else if (request.headers.get('X-Requested-With') === 'XMLHttpRequest')
     response = new NextResponse('Unauthorized', { status: 401 })
   else if (isF6Request) {
-    const timeSinceLastNormalRequest = lastNormalRequestTime ? (new Date().getTime() - lastNormalRequestTime.getTime()) : undefined
     logger.info(`Unauthenticated access from Win7 client (probably f6.ru) with IP ${request.headers.get('X-Forwarded-For') ?? 'unknown'}`)
-    if (timeSinceLastNormalRequest === undefined) {
-      logger.warn('No normal requests have been recorded yet, shutting down to avoid further uptime.')
-      return process.exit(0)
-    }
-    else if (timeSinceLastNormalRequest > MAX_IDLE_TIME_MS) {
-      logger.warn(`All requests in the last ${MAX_IDLE_TIME_MS.toString()} ms are from Win7 clients (probably f6.ru). Exiting the process to avoid further uptime.`)
-      return process.exit(0)
-    }
-    else {
-      logger.debug(`Last normal request was at ${timeSinceLastNormalRequest.toString()} ms ago, allowing this request.`)
-      response = new NextResponse('Unauthorized', { status: 401 })
-    }
+    response = new NextResponse('Unauthorized', { status: 401 })
   }
   else {
     const redirectTo = await startLogin(request)
