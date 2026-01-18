7752ea3bba5514f8b14979d7cc3fe5846fb058b9
lizzyTheLizard <14069652+lizzyTheLizard@users.noreply.github.com>
2026-01-18 22:44:57 +0100

    Fix Build

--- app/cash/[project_id]/[period]/reports/page.tsx
+++ app/cash/[project_id]/[period]/reports/page.tsx
@@ -1,12 +1,12 @@
 import { serverPageFunction } from '@/app/shared/PageFunction'
-  
+
 export const metadata = {
   title: 'Cash - Reports',
 }
 
 export default function Page() {
-  return serverPageFunction(metadata.title, async () => {
+  return serverPageFunction(metadata.title, () => {
   // TODO: Implement reports page
-  return 'To be implemented'
-  }
+    return 'To be implemented'
+  })
 }
