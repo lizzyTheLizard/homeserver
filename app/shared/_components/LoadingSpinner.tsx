2afbd855e9a60c8d7333892c0a0d9feb3c256021
lizzyTheLizard <14069652+lizzyTheLizard@users.noreply.github.com>
2026-01-23 17:47:46 +0100

    Reverted commit: Only show one loading spinner

--- app/shared/_components/LoadingSpinner.tsx
+++ app/shared/_components/LoadingSpinner.tsx
@@ -1,33 +1,13 @@
-'use client'
-import { useEffect, useState } from 'react'
-import { v4 as randomUUID } from 'uuid'
 import style from './LoadingSpinner.module.css'
 
 export interface LoadingSpinnerProps {
   text?: string
 }
 
-let spinners: { id: string, show: () => void }[] = []
-
 /**
  * A simple loading spinner.
  */
 export function LoadingSpinner({ text }: LoadingSpinnerProps) {
-  const [id] = useState(randomUUID())
-  const [show, setShow] = useState(false)
-
-  useEffect(() => {
-    spinners.push({ id, show: () => { setShow(true) } })
-    spinners[0].show()
-    return () => {
-      spinners = spinners.filter(sp => sp.id !== id)
-      spinners[0]?.show()
-      setShow(false)
-    }
-  }, [id, setShow])
-
-  if (!show) return null
-
   return (
     <>
       <div className={style.backdrop}></div>
