77af20d9644a66d866491cf297cf7a02a801a4e5
lizzyTheLizard <14069652+lizzyTheLizard@users.noreply.github.com>
2026-01-29 21:39:23 +0100

    Improved Period to contain date and openend. Use Temporal for computations

--- app/cash/_helper/Period.ts
+++ app/cash/_helper/Period.ts
@@ -1,10 +1,14 @@
+import { Temporal } from '@js-temporal/polyfill'
+
 export interface Period {
-  readonly current: boolean
+  readonly current?: boolean
+  readonly openEnded?: boolean
   readonly year?: number
   readonly month?: number
+  readonly day?: number
 }
 
-export const all: Period = { current: false }
+export const all: Period = { }
 
 export function stringToPeriod(s: string): Period {
   if (s === 'ALL') return all
@@ -13,25 +17,38 @@ export function stringToPeriod(s: string): Period {
     return { current: true, year: date.getFullYear(), month: date.getMonth() + 1 }
   }
   const parts = s.split('-').map(part => parseInt(part))
-  return { current: false, year: parts[0], month: parts[1] }
+  return { current: false, year: parts[0], month: parts[1], day: parts[2], openEnded: s.endsWith('+') }
 }
 
 export function periodToString(period: Period): string {
   if (period.current) return 'CURRENT'
   if (period.year === undefined) return 'ALL'
-  if (period.month === undefined) return period.year.toString().padStart(4, '0')
-  return `${period.year.toString().padStart(4, '0')}-${period.month.toString().padStart(2, '0')}`
+  let result = period.year.toString().padStart(4, '0')
+  if (period.month === undefined)
+    return `${result}${period.openEnded ? '+' : ''}`
+  result += `-${period.month.toString().padStart(2, '0')}`
+  if (period.day === undefined)
+    return `${result}${period.openEnded ? '+' : ''}`
+  result += `-${period.day.toString().padStart(2, '0')}`
+  return `${result}${period.openEnded ? '+' : ''}`
 }
 
 export function startDate(period: Period): string {
-  if (period.year === undefined) return '0001-01-01'
-  if (period.month === undefined) return period.year.toString().padStart(4, '0') + '-01-01'
-  return period.year.toString().padStart(4, '0') + '-' + period.month.toString().padStart(2, '0') + '-01'
+  return temporalFromPeriod(period).toString()
 }
 
 export function endDate(period: Period): string {
-  if (period.year === undefined) return '9999-12-31'
-  if (period.month === undefined) return (period.year + 1).toString().padStart(4, '0') + '-01-01'
-  if (period.month < 12) return period.year.toString().padStart(4, '0') + '-' + (period.month + 1).toString().padStart(2, '0') + '-01'
-  return (period.year + 1).toString().padStart(4, '0') + '-' + '01-01'
+  if (period.year === undefined || period.openEnded) return '9999-12-31'
+  const start = temporalFromPeriod(period)
+  if (period.month === undefined) return start.add({ years: 1 }).toString()
+  if (period.day === undefined) return start.add({ months: 1 }).toString()
+  return start.add({ days: 1 }).toString()
+}
+
+function temporalFromPeriod(period: Period): Temporal.PlainDate {
+  return Temporal.PlainDate.from({
+    year: period.year ?? 1,
+    month: period.month ?? 1,
+    day: period.day ?? 1 },
+  )
 }
